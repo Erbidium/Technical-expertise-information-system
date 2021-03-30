6c10a004f5dc3b4caf21fbdd194febd27b895a84
Oleksandr <OlexandrTanasienko@gmail.com>
2021-03-30 23:05:46 +0300

    Write first prototype of menu

---
+++ TechnicalExpertise/TechnicalExpertise/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int choice;
+	do
+	{
+		cout<<"Login(1) Register(2) Quit(0): ";
+		cin>>choice;
+		switch(choice)
+		{
+		case 1:
+			cout<<"Succesfully logined!"<<endl;
+			break;
+		case 2:
+			cout<<"Choose type of user: ";
+		default:
+			cout<<"Exit!..."<<endl;
+			return 0;
+		}
+	}
+	while(choice!=0);
+}
