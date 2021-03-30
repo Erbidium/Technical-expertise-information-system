0be5f5a7d91ebfd2c550ec840a2ed70e53f8064b
Oleksandr <OlexandrTanasienko@gmail.com>
2021-03-30 23:31:44 +0300

    Add choosing type of user and action

--- TechnicalExpertise/TechnicalExpertise/main.cpp
+++ TechnicalExpertise/TechnicalExpertise/main.cpp
@@ -11,14 +11,38 @@ int main()
 		cin>>choice;
 		switch(choice)
 		{
-		case 1:
-			cout<<"Succesfully logined!"<<endl;
-			break;
-		case 2:
-			cout<<"Choose type of user: ";
-		default:
+		case 0:
 			cout<<"Exit!..."<<endl;
 			return 0;
+		case 1:
+			int typeOfUser;
+			cout<<"(0)Grant recipient (1)Expert comission (2)Fund owner (3)Admin (4)Exit: ";
+			cin>>typeOfUser;
+			int ID;
+			cout<<"Enter account ID: ";
+			cin>>ID;
+			cout<<"Succesfully logined!"<<endl;
+			int action;
+			switch(typeOfUser)
+			{
+			case 0:
+				cout<<"Choose action!"<<endl;
+				cout<<"(0)Create an application\n(1)Check the status of application\n(2)Delete the application: ";
+				cin>>action;
+				break;
+			case 1:
+				cout<<"Choose action!"<<endl;
+				cout<<"(0)View the application\n(1)Evaluate the application(2): ";
+			case 4:
+				cout<<"Exit!..."<<endl;
+				return 0;
+			}
+			break;
+		case 2:
+			typeOfUser=-1;
+			cout<<"(0)Grant recipient (1)Expert comission (2)Fund owner (3)Exit: ";
+			cin>>typeOfUser;
+			break;
 		}
 	}
 	while(choice!=0);
