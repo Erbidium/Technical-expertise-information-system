de81b3ac0b0bce70f455fe69fcbcd8d5b265998e
Oleksandr <OlexandrTanasienko@gmail.com>
2021-03-30 21:59:55 +0300

    Implement getters and setters of class Application

--- TechnicalExpertise/TechnicalExpertise/Application.cpp
+++ TechnicalExpertise/TechnicalExpertise/Application.cpp
@@ -1,1 +1,62 @@
 #include "Application.h"
+
+string Application::getName()const
+{
+	return ownerName;
+}
+
+int Application::getAge()const
+{
+	return ownerAge;
+}
+
+string Application::getContent()const
+{
+	return applicationContent;
+}
+
+string Application::getDate()const
+{
+	return applicationDate;
+}
+
+string Application::getReview() const
+{
+	return comissionReview;
+}
+
+bool Application::getGrantConfirm()const
+{
+	return grantIsConfirmed;
+}
+
+void Application::setName(const string & ownerName)
+{
+	this->ownerName=ownerName;
+}
+
+void Application::setAge(const int& ownerAge)
+{
+	this->ownerAge=ownerAge;
+}
+
+void Application::setContent(const string& applicationContent)
+{
+	this->applicationContent=applicationContent;
+}
+
+void Application::setDate(const string& applicationDate)
+{
+	this->applicationDate=applicationDate;
+}
+
+void Application::setGrantConfirm(bool grantIsConfirmed)
+{
+	this->grantIsConfirmed=grantIsConfirmed;
+}
+
+void Application::setReview(const string& comissionReview)
+{
+	this->comissionReview=comissionReview;
+}
+
--- TechnicalExpertise/TechnicalExpertise/Application.h
+++ TechnicalExpertise/TechnicalExpertise/Application.h
@@ -5,17 +5,18 @@ using namespace std;
 class Application
 {
 public:
-	string getName();
-	int getAge();
-	string getContent();
-	string getDate();
-	bool getGrantConfirm();
-	void setName(string);
-	void setAge(int);
-	void setContent(string);
-	void setDate(string);
+	string getName()const;
+	int getAge()const;
+	string getContent()const;
+	string getDate()const;
+	string getReview()const;
+	bool getGrantConfirm()const;
+	void setName(const string&);
+	void setAge(const int&);
+	void setContent(const string&);
+	void setDate(const string&);
 	void setGrantConfirm(bool);
-	void setReview(string);
+	void setReview(const string&);
 private:
 	string ownerName;
 	int ownerAge;
