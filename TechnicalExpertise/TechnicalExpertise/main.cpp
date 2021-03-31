fc743b254fa99226ff9b961365425f08b86d8e58
Valid <validhernufKPI@gmail.com>
2021-03-31 19:30:06 +0300

    Remove usage of <filesystem> in main

--- TechnicalExpertise/TechnicalExpertise/main.cpp
+++ TechnicalExpertise/TechnicalExpertise/main.cpp
@@ -2,16 +2,25 @@
 #include <ctime>
 #include <filesystem>
 #include <string>
+#include <sys\stat.h>
 #include  "SiteInterface.h"
-
 using namespace std;
-namespace fs = filesystem;
+//namespace fs = filesystem;
 int main()
 {
 	srand(time(NULL));
-	fs::create_directory("Database");
-	fs::create_directory("Database/Applications");
-	fs::create_directory("Database/Profiles");
-	fs::create_directory("Database/Requests");
+	struct stat buf;
+	string filerep[5] = {"Database","Database/Applications","Database/Profiles","Database/Requests"};
+	for (int i = 0; i < 5; i++)
+	{
+		if ((stat(filerep[i].data(), &buf))!= 0)
+		{
+			mkdir(filerep[i].data());
+		}
+	}
+	//fs::create_directory("Database");
+	//fs::create_directory("Database/Applications");
+	//fs::create_directory("Database/Profiles");
+	//fs::create_directory("Database/Requests");
 	SiteInterface::showMenu();
 }
