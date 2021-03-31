bb890317f91b8c76063786bd571a7af398f4be8b
Oleksandr <OlexandrTanasienko@gmail.com>
2021-03-31 17:23:18 +0300

    Add creation of folder with userID when profile is created

--- TechnicalExpertise/TechnicalExpertise/ApplyTheRegistration.cpp
+++ TechnicalExpertise/TechnicalExpertise/ApplyTheRegistration.cpp
@@ -1,8 +1,10 @@
 #include "ApplyTheRegistration.h"
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 
 using namespace std;
+namespace fs = filesystem;
 
 void ApplyTheRegistration::profileManager()
 {
@@ -10,6 +12,7 @@ void ApplyTheRegistration::profileManager()
 	ofstream outProfile("Database/Profiles/"+std::to_string(profileData.getID())+".txt");
 	Accounts<<profileData.getPassword()<<" "<<profileData.getLogin()<<" "<<profileData.getID()<<"\n";
 	outProfile<<profileData.getName()<<" "<<profileData.getEmail()<<" "<<profileData.getType()<<"\n";
+	fs::create_directory("Database/Applications/"+to_string(profileData.getID()));
 	outProfile.close();
 	Accounts.close();
 }
